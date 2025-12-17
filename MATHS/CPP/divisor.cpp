0b78c1f103862c2ca88312c2bb1f1f4de501d40e
Sahil Das <125433719+sahilDas04@users.noreply.github.com>
2025-12-17 11:49:38 +0530

    intial commit

--- MATHS/CPP/divisor.cpp
+++ MATHS/CPP/divisor.cpp
@@ -1,13 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// void divisor(int n){
+//     for(int i=1; i<=n; i++){
+//         if(n % i  == 0){
+//             cout<<i<<" ";
+//         }
+//     }
+// }
+
 void divisor(int n){
-    for(int i=1; i<=n; i++){
+    vector<int> ls;
+    for(int i=1; i<=sqrt(n); i++){
         if(n % i  == 0){
-            cout<<i<<" ";
+            ls.push_back(i);
+            if((n / i) != i){
+                ls.push_back(n / i);
+            }
         }
     }
-}
+    sort(ls.begin(), ls.end());
+    for(auto i :  ls) cout<<i<<" ";
+} 
 
 int main(){
     divisor(36);
