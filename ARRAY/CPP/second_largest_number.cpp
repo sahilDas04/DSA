2e1a54821a7c5e8c27f831fa81efe4ebb60e875b
Sahil Das <125433719+sahilDas04@users.noreply.github.com>
2025-12-18 11:05:27 +0530

    intial commit

--- ARRAY/CPP/second_largest_number.cpp
+++ ARRAY/CPP/second_largest_number.cpp
@@ -2,23 +2,35 @@
 using namespace std;
 
 int second_largest(vector<int> nums){
-    int maxi = INT_MIN;
-    int s_maxi = INT_MIN;
+    // int maxi = INT_MIN;
+    // int s_maxi = INT_MIN;
+    // int n = nums.size();
+
+    // for(int i=0; i<n; i++){
+    //     if(nums[i] > maxi){
+    //         maxi = nums[i];
+    //     }
+    // }
+
+    // for(int i=0; i<n; i++){
+    //     if(nums[i] > s_maxi && nums[i] < maxi){
+    //         s_maxi = nums[i];
+    //     }
+    // }
+
+    // return s_maxi;
+
     int n = nums.size();
+    sort(nums.begin(), nums.end());
 
-    for(int i=0; i<n; i++){
-        if(nums[i] > maxi){
-            maxi = nums[i];
+    int largest = nums[n-1];
+
+    for(int i=n-2; i>=0; i--){
+        if(nums[i] != largest){
+            return nums[i];
         }
     }
-
-    for(int i=0; i<n; i++){
-        if(nums[i] > s_maxi && nums[i] < maxi){
-            s_maxi = nums[i];
-        }
-    }
-
-    return s_maxi;
+    return -1;
 }
 
 int main(){
